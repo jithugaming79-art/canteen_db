dfdaa0f54bca74034feabe8d45355eabea5bf64f
Jithu <jithu@campusbites.com>
2026-03-02 15:40:55 +0530

    Fix DB_PASSWORD default for Render deployment

--- canteen/settings.py
+++ canteen/settings.py
@@ -98,7 +98,7 @@ else:
             'ENGINE': 'django.db.backends.mysql',
             'NAME': config('DB_NAME', default='canteen_db'),
             'USER': config('DB_USER', default='root'),
-            'PASSWORD': config('DB_PASSWORD'),
+            'PASSWORD': config('DB_PASSWORD', default=''),
             'HOST': config('DB_HOST', default='localhost'),
             'PORT': config('DB_PORT', default='3306'),
             'OPTIONS': {
